e2ee8732ba725901fde8bc5d3d8aa46d8a1ebb68
Tal Arbatov <talabatov98@gmail.com>
2018-07-20 11:44:47 +0200

    config.js fixed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'LU4CT1BkFWy3bvLefj8MePPiX',
-    consumer_secret:      'a8IRBdiZcYRj8l32xBJtvlH5M4aSfbrfc3TQOH574KSObLpqxo',
-    access_token:         '1020220734965276673-VQF7nRe8xQ0Q5yU6RiuTJvdDpsKJBI',
-    access_token_secret:  'c78Fb418ihiVHvo9PvZvANx8EQXMwU3G0UW9yNJCUaAz4',
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
 }
